69c3926f74d06ff46f6a6a859d7aaca9be223e5e
AriBora <arindom21@iitk.ac.in>
2023-07-29 19:05:43 +0530

    Bugs fixed

--- src/get_graphs.py
+++ src/get_graphs.py
@@ -12,11 +12,11 @@ def get_graphs(pred, test_y):
     logging.info(test_y[:5])
     graphs_loc = os.path.join(folder_path,'graphs')
     for i in range(pred.shape[1]):
-        x = pred[:,i,:]
-
-        y = test_y[:,i,:]
+        plt.figure(figsize=(14,4))
+        x = pred[:,i,4]
+        y = test_y[:,i,4]
         plt.plot(x, 'r', label = 'Prediction')
-        plt.plot(y, 'b', label = 'Actual').reset_index()
+        plt.plot(y, 'b', label = 'Actual')
         s = 'stock' + str(i)+'.jpg'
         graph_loc = os.path.join(graphs_loc, s)
         plt.savefig(graph_loc)
